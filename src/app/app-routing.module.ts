f394f27558718e7874abe911fdbff64e48acadd1
ganesh-kamble-3032 <ganeshkamble9674@gmail.com>
2024-10-04 14:22:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RegistrationsComponent } from './components/registrations/registrations.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'menu',component:MenuComponent
+  },
+  {
+    path:'registrations',component:RegistrationsComponent
+  },
+  {
+    path:'footer',component:FooterComponent
+  },
+  {
+    path:'booking',component:BookingComponent
+  },
+  {
+    path:'feedback',component:FeedbackComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationsComponent } from './components/registrations/registrations.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { BookingComponent } from './components/booking/booking.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    LoginComponent,
+    RegistrationsComponent,
+    FooterComponent,
+    BookingComponent,
+    ContactusComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
